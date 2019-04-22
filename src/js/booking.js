d503c678115baa8d475e30d3ba9f05f15eca84a9
zlayabekrija <zlaja.bekrija@gmail.com>
2019-04-22 13:44:20 +0200

    'form_validation'

--- src/js/booking.js
+++ src/js/booking.js
@@ -16,22 +16,26 @@ const booking = () => {
     submit.textContent = 'Book';
     submit.classList.add('btn','btn-primary');
     name.setAttribute('type','text');
+    name.setAttribute("required", "");
     name.placeholder = 'Your name'
     name.classList.add('form-control');
     const email = document.createElement('input');
     email.setAttribute('type','email');
     email.placeholder = 'Your email';
     email.classList.add('form-control');
+    email.setAttribute("required", "");
     const number = document.createElement('input');
     number.setAttribute('type','number');
     number.setAttribute('min','1');
     number.setAttribute('max','10');
     number.placeholder = 'Number of guests (1-10)';
     number.classList.add('form-control');
+    number.setAttribute("required", "");
     const date = document.createElement('input');
     date.setAttribute('type','datetime-local');
     date.placeholder = 'Date and Time';
     date.classList.add('form-control');
+    date.setAttribute("required", "");
     field.appendChild(name);
     field.appendChild(email);
     field.appendChild(number);
@@ -45,6 +49,12 @@ const booking = () => {
 }
 const booked = () =>{
    const inputs = document.querySelectorAll('input');
+   for (let i = 0; i < inputs.length; i++){
+       if (inputs[i].value === ''){
+        alert('Plese Fill out all fields');
+        return;
+       }
+   }
    alert(`Dear ${inputs[0].value}, your reservation has been booked
     you will recive an email shortly on ${inputs[1].value}.
     You have made reservation for ${inputs[2].value}, on ${inputs[3].value}.
